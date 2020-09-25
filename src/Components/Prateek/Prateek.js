c3cc0374853ef9218abeba6cb427796461a153ec
prateekJani <58832930+prateekJani@users.noreply.github.com>
2020-09-25 01:41:28 -0400

    Create Prateek.js

---
+++ src/Components/Prateek/Prateek.js
@@ -0,0 +1,17 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import './Prateek.css'
+
+function Prateek() {
+
+
+  const name = "Prateek";
+
+  return (
+    <p className={"paragraph"}>
+     Hello {name}
+    </p>
+  );
+}
+
+export default Prateek;
