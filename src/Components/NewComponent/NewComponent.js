03053c091706f9f771551ff67050f25dc538ce1f
prateekJani <58832930+prateekJani@users.noreply.github.com>
2020-09-29 19:46:13 -0400

    Create NewComponent.js

---
+++ src/Components/NewComponent/NewComponent.js
@@ -0,0 +1,16 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import './NewComponent.css'
+
+function NewComponent() {
+
+  const name = "Prateek";
+
+  return (
+    <p id = 'newComponent'>
+        "Hello New Component"
+    </p>
+  );
+}
+
+export default NewComponent;
